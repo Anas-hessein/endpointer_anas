a5465fdb5d0da5016e5871465d047b3a7b0a94ec
Anas-hessein <anas.hessen15@gmail.com>
2025-08-15 10:02:06 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("✅ Connected to MongoDB Atlas"))
+    .catch(err => console.error(err));
+
+const recipeSchema = new mongoose.Schema({
+    title: String,
+    ingredients: [String],
+    instructions: String
+});
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+app.post("/recipes", async (req, res) => {
+    const recipe = new Recipe(req.body);
+    await recipe.save();
+    res.json({ message: "Recipe added!"});
+});
+
+app.get("/recipes", async (req, res) => {
+    const recipes = await Recipe.find();
+    res.json(recipes);
+});
+
+app.put("/recipes/:id", async (req, res) => {
+    await Recipe.findByIdAndUpdate(req.params.id, req.body);
+    res.json({ message: "Recipe updated!"});
+});
+
+app.delete("/recipes/:id", async (req, res) => {
+    await Recipe.findByIdAndDelete(req.params.id);
+    res.json({ message: "Recipe deleted!"});
+});
+
+app.listen(3000, () => console.log("🚀 Server running on port 3000"));
