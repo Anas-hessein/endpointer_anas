c4e4172fabacb0eea2e3555c9067b3a1853e8910
Anas-hessein <anas.hessen15@gmail.com>
2025-08-18 03:48:16 +0200

    new commit

--- pages/api-docs.js
+++ pages/api-docs.js
@@ -1,7 +1,6 @@
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
-import swaggerSpec from "../swagger.json";
 
 export default function ApiDocs() {
-  return <SwaggerUI spec={swaggerSpec} />;
+  return <SwaggerUI url="/swagger.json" />;
 }
